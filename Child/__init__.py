e6e1ea4091dc0f157ab810a3ffd8b34d88510e5c
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2026-03-20 17:52:25 +0000

    remove

--- Child/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-The child code is the model itself that starts empty and is guided by the parent 
-"""
-
--- Parent/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-The parent code is the model that helps guide the child model into learning rules
-
-"""
-import numpy as np 
